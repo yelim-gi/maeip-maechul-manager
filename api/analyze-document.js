3b5370011afbaf9f687c63fbbb7bed6217b53aab
Ye-Lim <qzwxec88888@gmail.com>
2026-05-26 20:47:15 +0900

    remove response mime type

--- api/analyze-document.js
+++ api/analyze-document.js
@@ -73,7 +73,6 @@ export default async function handler(req, res) {
         body: JSON.stringify({
           generationConfig: {
             temperature: 0.1,
-            responseMimeType: 'application/json'
           },
           contents: [
             {
